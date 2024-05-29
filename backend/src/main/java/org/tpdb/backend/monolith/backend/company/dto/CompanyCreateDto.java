7bf2ee6321e062ad9f1707b1202b7c7a006572b1
BasBrunink <115614124+BasBrunink@users.noreply.github.com>
2024-05-29 11:05:44 +0200

    [WIP] Dto Created

--- backend/src/main/java/org/tpdb/backend/monolith/backend/company/dto/CompanyCreateDto.java
+++ backend/src/main/java/org/tpdb/backend/monolith/backend/company/dto/CompanyCreateDto.java
@@ -1,4 +1,17 @@
 package org.tpdb.backend.monolith.backend.company.dto;
 
+import org.tpdb.backend.monolith.backend.common.enums.OperationalStatus;
+import org.tpdb.backend.monolith.backend.company.CompanyType;
+
+import java.time.LocalDate;
+
 public class CompanyCreateDto {
+  private String name;
+  private String description;
+  private String website;
+  private String email;
+  private CompanyType companyType;
+  private LocalDate foundingDate;
+  private LocalDate closingDate;
+  private OperationalStatus operationalStatus;
 }
