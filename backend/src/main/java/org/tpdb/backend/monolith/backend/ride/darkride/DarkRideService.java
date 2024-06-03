933b962748570e7d739fc4bb93072e7919dd907b
BasBrunink <115614124+BasBrunink@users.noreply.github.com>
2024-06-03 10:32:54 +0200

    [WIP] Minor changes added

--- backend/src/main/java/org/tpdb/backend/monolith/backend/ride/darkride/DarkRideService.java
+++ backend/src/main/java/org/tpdb/backend/monolith/backend/ride/darkride/DarkRideService.java
@@ -1,4 +1,21 @@
 package org.tpdb.backend.monolith.backend.ride.darkride;
 
+import org.springframework.stereotype.Service;
+import org.tpdb.backend.monolith.backend.ride.models.DarkRide;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
 public class DarkRideService {
+
+  private final DarkRideRepository darkRideRepository;
+
+  public DarkRideService(DarkRideRepository darkRideRepository) {
+    this.darkRideRepository = darkRideRepository;
+  }
+
+  public Optional<DarkRide> getById(UUID id) {
+    return darkRideRepository.findById(id);
+  }
 }
--- backend/src/main/java/org/tpdb/backend/monolith/backend/ride/models/attributes/darkridescene/dto/DarkRideSceneCreateDto.java
+++ backend/src/main/java/org/tpdb/backend/monolith/backend/ride/models/attributes/darkridescene/dto/DarkRideSceneCreateDto.java
@@ -1,7 +1,14 @@
 package org.tpdb.backend.monolith.backend.ride.models.attributes.darkridescene.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DarkRideSceneCreateDto {
   private UUID darkRideId;
   private String name;
